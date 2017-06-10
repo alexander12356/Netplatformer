472d3694f50992ce9a9c8d37db3bb203cd4043f7
Aisen <l1sten51091@gmail.com>
2017-06-10 16:05:57 +0900

    Deattached things

---
+++ Assets/test/DeattachedTest_Limb.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class DeattachedTest_Limb : MonoBehaviour {
+
+    public IEnumerator FadeTo (float initValue, float duration, float timeUntilStart){
+        yield return new WaitForSeconds(timeUntilStart);
+        SpriteRenderer sprtrndr = GetComponent<SpriteRenderer>();
+        Color newColor = new Color(1, 1, 1, 0);
+        sprtrndr.color = newColor;
+        float alpha = sprtrndr.color.a;
+
+        for (float i = 0; i < 1; i+= Time.deltaTime / duration)
+        {
+            newColor = new Color(1, 1, 1, Mathf.Lerp(initValue, alpha, i));
+            sprtrndr.color = newColor;
+
+            if (i > .98f){
+                gameObject.SetActive(false);
+            }
+            yield return null;
+        }
+    }
+}
