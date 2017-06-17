d35fef9846d6b6613cc3df8e79f3795e9648e5d6
Aisen <l1sten51091@gmail.com>
2017-06-17 16:22:38 +0900

    Fatality first commit

---
+++ Assets/Eternal/Scripts/FatalityManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharacterSystem
+{
+    public class FatalityManager : MonoBehaviour
+    {
+        private static FatalityManager m_Instance;
+
+        public GameObject fatalityAnimator;
+        Animator m_Animator = new Animator();
+
+        public static FatalityManager Instance()
+        {
+            return m_Instance;
+        }
+
+        private void Awake()
+        {
+            if (m_Instance != null)
+            {
+                return;
+            }
+            else
+            {
+                m_Instance = this;
+            }
+        }
+
+        public void PlayFatality(GameObject winner, GameObject loser, Transform holderParent = null)
+        {
+            CharacterCompositor winnerCharacter = winner.GetComponent<CharacterCompositor>(); //TODO взять другой класс
+            CharacterCompositor loserCharacter = loser.GetComponent<CharacterCompositor>();
+			
+            if (holderParent != null)
+				fatalityAnimator.gameObject.transform.SetParent(holderParent, false);
+			
+			winner.transform.SetParent(fatalityAnimator.gameObject.transform, false);
+			loser.transform.SetParent(fatalityAnimator.gameObject.transform, false);
+
+            GameObject _delta = Instantiate(fatalityAnimator) as GameObject;
+            m_Animator = _delta.GetComponent <Animator > ();
+            m_Animator.SetTrigger("Fatality"); //TODO тип оружия
+        }
+
+        public void StopFatality()
+        {
+            m_Animator.StopPlayback();
+        }
+    }
+}
